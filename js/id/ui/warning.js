0ec97067816bfbd89079300c37ce05430d13d916
sisskind <josh.sisskind@digitalglobe.com>
2015-09-24 07:11:11 -0400

    Warning symbol is now yellow * Removes previous image in the event it is not cleared

--- js/id/ui/warning.js
+++ js/id/ui/warning.js
@@ -1,6 +1,7 @@
 iD.ui.Warning = function(context,status,message) {
     return function(selection) {
         if(status){
+        	selection.selectAll('img').remove();
         	selection.style('opacity',1.0);
         	selection.append('img')
 	            .attr('src', context.imagePath('warning.png'))
