9658ce8fef1561bc5b5c591b13d1725fa61cd539
sisskind <josh.sisskind@digitalglobe.com>
2015-10-06 07:27:50 -0400

    User can define processing text

--- js/id/ui/processing.js
+++ js/id/ui/processing.js
@@ -1,13 +1,16 @@
-iD.ui.Processing = function(context,status) {
+iD.ui.Processing = function(context,status,txt) {
     return function(selection) {
-        if(status){
+        if(!txt){txt='Processing...'}
+    	
+    	if(status){
         	var modalbg = selection.append('div')
             	.attr('id','processingDiv')
             	.classed('fill-darken3 pin-top pin-left pin-bottom pin-right', true);
         	var processingDiv = modalbg.append('div')
             	.classed('contain col4 pad1 hoot-menu fill-white round modal', true);
         	processingDiv.append('h1')
-        		.text('Processing...');
+        		.style({'text-align': 'center'})
+        		.text(txt);
         	processingDiv.append('img')
             	.attr('src', context.imagePath('loader-white.gif'))
             	.style({'display': 'block','margin': 'auto'});
