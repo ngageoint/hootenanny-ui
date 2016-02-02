ab6fec2d164c516976eda685ea122193055036c3
sisskind <josh.sisskind@digitalglobe.com>
2016-02-02 16:49:54 -0500

    Need to style tab into rows

--- js/hoot/view/utilities/UISettings.js
+++ js/hoot/view/utilities/UISettings.js
@@ -13,41 +13,48 @@ Hoot.view.utilities.uisettings = function(context) {
             }
 
             var settings = [
-                {'label':'Line Width','default_option':true,'type':'number','name':'_linewidth','min':1,'max':25,'step':1,'value':7}
+                {'group':'Feature Rendering','id':'ui-settings-feature-rendering',items:[
+                    {'label':'Line Width','default_option':true,'type':'number','name':'_linewidth','min':1,'max':25,'step':1,'value':7}
+                ]
+                }
             ]
 
             var _fieldset = container.append('form').append('fieldset').selectAll('.form-field').data(settings);
             _fieldset.enter()
                 .append('div')
-                .classed('form-field form-field-lanes modified present',true)
+                .append('label').classed('pad1x pad0y strong fl', true)
+                .text(function(d){return d.group;})
                 .each(function(d){
-                    if(d.type=='number'){
-                        d3.select(this).append('label')
-                            .classed('form-label',true)
-                            .attr('for',d.name)
-                            .text(d.label);
-                        d3.select(this).append('input')
-                            .attr('type','text')
-                            .attr('id',d.name)
-                            .attr('placeholder','1');
-                        var spinControl = d3.select(this).append('div').classed('spin-control',true);
-                        d3.select('.spin-control').append('button').attr('class','increment');
-                        d3.select('.spin-control').append('button').attr('class','decrement');
-                    }
-                });
+                    var settingsGroup = d3.select(this.parentNode).append('div').attr('id',d.id);
+                    _.each(d.items,function(j){
+                        /*this.append('label').classed('settings-label fl',true).attr('for',j.name).text(j.label);
+                        var inpt = this.append('input').classed('hoot-input',true).attr('id',j.name).attr('placeholder','');
+                        if(j.type=='number'){
+                            inpt.attr('type','number').attr('min',j.min).attr('max',j.max);
+                        }*/
 
-                // Set click event for all spin-controls
-                d3.select('.spin-control').selectAll('button')
-                    .on('click', function(d) {
-                        d3.event.preventDefault();
-                        var input = d3.select('#' + d.name);
-                        var num = parseInt(input.node().value || 1, 10);
-                        if (isNaN(num)){return;}
-                        if(_.contains(this.classList,'increment')){input.node().value = Math.min(d.max,num+1);}
-                        else if(_.contains(this.classList,'decrement')){input.node().value = Math.max(d.min,num-1);}
-                    });
+                        var section = this.append('section');
+                        section.append('h3').text('test');
+                        var sectionDiv = section.append('div').append('div').classed('settings-row',true);
+                        sectionDiv.append('label').append('span').text('SPAN');
+                        sectionDiv.append('input').attr('type','number').classed('hoot-input',true).attr('id',j.name).attr('placeholder','');
+                    },settingsGroup)
+                })
+            ;
         };
 
+/*<section id="web-content-section">
+    <h3 i18n-content="advancedSectionTitleContent">Web content</h3>
+    <div>
+      <div class="settings-row">
+        <label class="web-content-select-label">
+          <span i18n-content="defaultZoomFactorLabel">Page zoom:</span>
+          <select id="defaultZoomFactor" datatype="double"><option value="0.25">25%</option><option value="0.333">33%</option><option value="0.5">50%</option><option value="0.666">67%</option><option value="0.75">75%</option><option value="0.9">90%</option><option value="1">100%</option><option value="1.1">110%</option><option value="1.25">125%</option><option value="1.5">150%</option><option value="1.75">175%</option><option value="2">200%</option><option value="2.5">250%</option><option value="3">300%</option><option value="4">400%</option><option value="5">500%</option></select>
+        </label>
+      </div>
+
+    </div>
+  </section>*/
 
 	return hoot_view_utilities_uisettings;
 }
