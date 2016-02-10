24fd99afc523dcf98e670a12c8ec50dbfe0daac2
sisskind <josh.sisskind@digitalglobe.com>
2016-02-10 15:31:46 -0500

    Moved line width adjustment to sidebar * #223

--- js/hoot/view/utilities/Utilities.js
+++ js/hoot/view/utilities/Utilities.js
@@ -11,7 +11,6 @@ Hoot.view.utilities = function (context){
 	utilities.errorlog = Hoot.view.utilities.errorlog(context);
     utilities.reports = Hoot.view.utilities.reports(context);
     utilities.about = Hoot.view.utilities.about(context);
-    utilities.uisettings = Hoot.view.utilities.uisettings(context);
     utilities.reviewbookmarks = Hoot.view.utilities.reviewbookmarks(context);
     utilities.reviewbookmarknotes = Hoot.view.utilities.reviewbookmarknotes(context);
 
