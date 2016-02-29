c1c475cfd7fb5f8be6f0aec09c56b6edfa767550
sisskind <josh.sisskind@digitalglobe.com>
2016-02-29 17:14:45 -0500

    Entity tag values sorted a-z

--- js/id/ui/raw_tag_editor.js
+++ js/id/ui/raw_tag_editor.js
@@ -193,6 +193,10 @@ iD.ui.RawTagEditor = function(context) {
             function sort(value, data) {
                 var sameletter = [],
                     other = [];
+                data.sort(function(a,b){var textA = a.value.toUpperCase();
+                    var textB = b.value.toUpperCase();
+                    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+                });
                 for (var i = 0; i < data.length; i++) {
                     if (data[i].value.substring(0, value.length) === value) {
                         sameletter.push(data[i]);
