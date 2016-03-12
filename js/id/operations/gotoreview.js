bc417dce5e936ce9cc3263d919e0fd3662d401f1
sisskind <josh.sisskind@digitalglobe.com>
2016-03-12 16:42:52 -0500

    First cut to adding label * Clickable label will be added above POI if user selects GoTo option. * User will then click on label to drive to that review * An 'X' will also appear if the user wants to cancel

--- js/id/operations/gotoreview.js
+++ js/id/operations/gotoreview.js
@@ -3,10 +3,13 @@ iD.operations.Gotoreview = function(selectedIDs, context) {
         entity = context.entity(entityId),
         extent = entity.extent(context.graph()),
         geometry = context.geometry(entityId),
-        action = iD.actions.Review(entityId, context.projection);
+        action = iD.actions.Review(entityId, context.projection),
+        rectMargin=30;
 
     var operation = function() {
-        var annotation = 'Goto related review item';
+        var annotation = 'Go to related review item';
+
+        _performHighlight(context.graph());
     };
 
     operation.available = function() {
@@ -19,14 +22,57 @@ iD.operations.Gotoreview = function(selectedIDs, context) {
     };
 
     operation.tooltip = function() {
-        return 'Goto related review item.';
+        return 'Go to related review item.';
     };
 
+    var _selectReview = function(graph) {
+        // Create a label above all POIs in relation
+        var svg = d3.select('.layer-label');
+
+        var feature = context.hasEntity(entityId);
+        graph.parentRelations(feature)
+            .forEach(function(parent) {
+                _.each(parent.members, function(mem){
+                    var mid = mem.id;
+
+                    var mFeature = context.hasEntity(mid);
+                    if(mFeature && (entityId != mid)) {
+                        var coord = [ feature.loc, mFeature.loc];
+                        console.log(coord);
+
+                        //take this coord, convert to SVG, add to map
+
+                    }
+                        
+                });
+            });
+
+        
+        var g = svg.append('g');
+        
+        //rect = g.append('rect').style('fill','black').style('fill-opacity','0.5');
+        label = g.append('text').style('fill','white').style('font-size','18px');        
+
+        lengthLabel = label.append("tspan").text("TEST");
+        
+        c = [200,200];
+
+        /*rect.attr("x", c[0]+10)
+            .attr("y", c[1]-(label.dimensions()[1]/2))
+            .attr("width",label.dimensions()[0]+5)
+            .attr("height",label.dimensions()[1]+5);*/
+
+        label.attr("x", c[0]+rectMargin)
+            .attr("y", c[1]+rectMargin);
+        lengthLabel.attr("x", c[0]+10)
+            .attr("y", c[1])
+            .text(function(d) { return 'test'});
+    }
     
 
     operation.id = 'gotoreview';
     operation.keys = ['G'];
-    operation.title = 'Goto Review';
+    operation.title = 'Go to Review';
 
     return operation;
 };
