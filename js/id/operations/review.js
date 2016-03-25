89287c53d52a7e77f26c6ae072ae7c7283a785ea
sisskind <josh.sisskind@digitalglobe.com>
2016-03-25 14:28:09 -0400

    Resolved reviews do not get bubble

--- js/id/operations/review.js
+++ js/id/operations/review.js
@@ -98,16 +98,18 @@ iD.operations.Review = function(selectedIDs, context) {
         var multiLines = [];
         graph.parentRelations(feature)
             .forEach(function(parent) {
-                _.each(parent.members, function(mem){
-                    var mid = mem.id;
+                if(parent.tags['hoot:review:needs']!='no'){
+                    _.each(parent.members, function(mem){
+                        var mid = mem.id;
 
-                    var mFeature = context.hasEntity(mid);
-                    if(mFeature && (entityId != mid)) {
-                        mFeatureLoc = _getLocation(mFeature);
-                        var coord = [ featureLoc,mFeatureLoc];
-                        multiLines.push(coord);
-                    }                        
-                });
+                        var mFeature = context.hasEntity(mid);
+                        if(mFeature && (entityId != mid)) {
+                            mFeatureLoc = _getLocation(mFeature);
+                            var coord = [ featureLoc,mFeatureLoc];
+                            multiLines.push(coord);
+                        }                        
+                    });
+                }
             });
 
         _loadReview('', multiLines);
@@ -144,59 +146,61 @@ iD.operations.Review = function(selectedIDs, context) {
 
         graph.parentRelations(feature)
             .forEach(function(parent) {
-                _.each(parent.members, function(mem){
-                    var mid = mem.id;
+                if(parent.tags['hoot:review:needs']!='no'){
+                    _.each(parent.members, function(mem){
+                        var mid = mem.id;
 
-                    var mFeature = context.hasEntity(mid);
-                    mFeatureLoc = _getLocation(mFeature);
+                        var mFeature = context.hasEntity(mid);
+                        mFeatureLoc = _getLocation(mFeature);
 
-                    var circleOffset = feature.type == 'node' ? 50 : 0;
+                        var circleOffset = feature.type == 'node' ? 50 : 0;
 
-                    if(mFeature && (entityId != mid)) {
-                        //take this coord, convert to SVG, add to map
-                        var c = context.projection(mFeatureLoc);
-                        var transform = 'translate('.concat(c[0],',',c[1]-circleOffset,')');
-                        var g = svg.append('g').attr('transform',transform).attr('loc',mFeatureLoc).classed('gotoreview _' + mFeature.type,true);
-                        g.append('circle').attr('r','20')
-                            .attr('stroke','white').attr('stroke-width','3')
-                            .attr('fill','green').attr('fill-opacity','0.5');
-                        g.append('text').attr('dx','-6').attr('dy','6')
-                            .style('fill','white').style('font-size','16px').attr('font-weight','bold')
-                            .text(function(){
-                                if(!doubleLetter){return String.fromCharCode(currentAlpha).toUpperCase();}
-                                else{return String.fromCharCode(currentAlpha).toUpperCase().concat(String.fromCharCode(currentAlpha).toUpperCase());}
+                        if(mFeature && (entityId != mid)) {
+                            //take this coord, convert to SVG, add to map
+                            var c = context.projection(mFeatureLoc);
+                            var transform = 'translate('.concat(c[0],',',c[1]-circleOffset,')');
+                            var g = svg.append('g').attr('transform',transform).attr('loc',mFeatureLoc).classed('gotoreview _' + mFeature.type,true);
+                            g.append('circle').attr('r','20')
+                                .attr('stroke','white').attr('stroke-width','3')
+                                .attr('fill','green').attr('fill-opacity','0.5');
+                            g.append('text').attr('dx','-6').attr('dy','6')
+                                .style('fill','white').style('font-size','16px').attr('font-weight','bold')
+                                .text(function(){
+                                    if(!doubleLetter){return String.fromCharCode(currentAlpha).toUpperCase();}
+                                    else{return String.fromCharCode(currentAlpha).toUpperCase().concat(String.fromCharCode(currentAlpha).toUpperCase());}
+                                });
+                            
+                            var reqParam = {
+                                'mapId':this.mapId,
+                                'sequence':this.tags['hoot:review:sort_order']
+                            };
+
+                            var _parent = function() {return context.hoot().control.conflicts;};
+
+                            g.on('click',function(){
+                                Hoot.model.REST('reviewGetReviewItem', reqParam, function (resp) {  
+                                    if(resp.error){
+                                        context.hoot().view.utilities.errorlog.reportUIError(d.error);
+                                        return;
+                                    } 
+
+                                    if(resp.resultCount < 1){
+                                      alert('The review item already has been resolved. Can not go to review item.');
+                                    } else {
+                                        _parent().actions.idgraphsynch.getRelationFeature(resp.mapId, resp.relationId, 
+                                        function(newReviewItem){
+                                            _parent().map.featurehighlighter.highlightLayer(newReviewItem.members[0], 
+                                                newReviewItem.members[1],false);
+                                        });                              
+                                    }
+                                });
                             });
-                        
-                        var reqParam = {
-                            'mapId':this.mapId,
-                            'sequence':this.tags['hoot:review:sort_order']
-                        };
 
-                        var _parent = function() {return context.hoot().control.conflicts;};
-
-                        g.on('click',function(){
-                            Hoot.model.REST('reviewGetReviewItem', reqParam, function (resp) {  
-                                if(resp.error){
-                                    context.hoot().view.utilities.errorlog.reportUIError(d.error);
-                                    return;
-                                } 
-
-                                if(resp.resultCount < 1){
-                                  alert('The review item already has been resolved. Can not go to review item.');
-                                } else {
-                                    _parent().actions.idgraphsynch.getRelationFeature(resp.mapId, resp.relationId, 
-                                    function(newReviewItem){
-                                        _parent().map.featurehighlighter.highlightLayer(newReviewItem.members[0], 
-                                            newReviewItem.members[1],false);
-                                    });                              
-                                }
-                            });
-                        });
-
-                        currentAlpha += 1;
-                        if(currentAlpha > 122){currentAlpha = 97; doubleLetter = true;}                      
-                    }  
-                },parent);
+                            currentAlpha += 1;
+                            if(currentAlpha > 122){currentAlpha = 97; doubleLetter = true;}                      
+                        }  
+                    },parent);
+                }
             });
     }
 
