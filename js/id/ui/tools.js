c3d8a6bb6c93ea38db92529f2151fff2c0418d78
sisskind <josh.sisskind@digitalglobe.com>
2016-04-13 13:54:37 -0400

    Use SVG not Icon

--- js/id/ui/tools.js
+++ js/id/ui/tools.js
@@ -5,79 +5,80 @@ iD.ui.Tools = function(context) {
     }
     
     function tools() {
-    	 // If in review mode, do not include Clip tools
+         // If in review mode, do not include Clip tools
          var items = [];
-         items.push({title:'Measurement Tools',icon:'add-line',group:'measure',items:[          
-                {title:'Measure Length',tooltip:'Shortcut: 6',group:'measure',type:'line',icon:'add-line',mode:iD.modes.MeasureAddLine(context)},
-                {title:'Measure Area',tooltip:'Shortcut: 7',group:'measure',type:'area',icon:'add-area',mode:iD.modes.MeasureAddArea(context)},
+         items.push({title:'Measurement Tools',icon:'line',group:'measure',items:[          
+                {title:'Measure Length',tooltip:'Shortcut: 6',group:'measure',type:'line',icon:'line',mode:iD.modes.MeasureAddLine(context)},
+                {title:'Measure Area',tooltip:'Shortcut: 7',group:'measure',type:'area',icon:'area',mode:iD.modes.MeasureAddArea(context)},
                 {title:'Measure Help',tooltip:'',group:'measure',type:'help',icon:'help',action:'measureHelp'},
             ]});
 
         if (!hoot.control.conflicts.isConflictReviewExist()) {
-            items.push({title:'Clip Tools',icon:'scissors',group:'clip',items:[
-                {title:'Clip to Visual Extent',tooltip:'Shortcut: 9',group:'clip',type:'area',icon:'scissors',action:'clipVisualExtent'},
-                {title:'Clip to Bounding Box',tooltip:'Shortcut: 8',group:'clip',type:'area',icon:'scissors',mode:iD.modes.ClipBoundingBox(context)}
+            items.push({title:'Clip Tools',icon:'clip',group:'clip',items:[
+                {title:'Clip to Visual Extent',tooltip:'Shortcut: 9',group:'clip',type:'area',icon:'clip',action:'clipVisualExtent'},
+                {title:'Clip to Bounding Box',tooltip:'Shortcut: 8',group:'clip',type:'area',icon:'clip',mode:iD.modes.ClipBoundingBox(context)}
             ]})
         };
         
         d3.select('html').append('div').attr('class', 'tools-menu');
                
         var toolsItem =  d3.selectAll('.tools-menu')
-    		.html('')
+            .html('')
             .append('ul')
             .selectAll('li')
             .data(items).enter()
             .append('li')
             .attr('class',function(item){return item.icon + ' tools-' + item.group;})
             .on('mouseenter',function(item){
-            	if(!item.items){return;}
-            	var itemHeight = d3.select('.tools-'+item.group).node().offsetTop+116+'px';
-            	d3.select('.tools-menu.sub-menu').remove();
-            	d3.select('html').append('div').attr('class','tools-menu sub-menu');
-            	var subTools = d3.selectAll('.tools-menu.sub-menu')
-            		.style('left', function(){
-            			var menuWidth = d3.select('.tools-menu').node().offsetWidth+1;
-            			return menuWidth+d3.select('button.tools').property('offsetLeft')+'px'||'0px'})
-            		.style('top', itemHeight)
-            		.style('display', 'block')	
-            		.html('')
-            		.append('ul')
-            		.selectAll('li')
-            		.data(item.items).enter()
-            		.append('li')
-            		.attr('class',function(item){
-            			return item.icon + ' tools-' + item.group;})
+                if(!item.items){return;}
+                var itemHeight = d3.select('.tools-'+item.group).node().offsetTop+116+'px';
+                d3.select('.tools-menu.sub-menu').remove();
+                d3.select('html').append('div').attr('class','tools-menu sub-menu');
+                var subTools = d3.selectAll('.tools-menu.sub-menu')
+                    .style('left', function(){
+                        var menuWidth = d3.select('.tools-menu').node().offsetWidth+1;
+                        return menuWidth+d3.select('button.tools').property('offsetLeft')+'px'||'0px'})
+                    .style('top', itemHeight)
+                    .style('display', 'block')  
+                    .html('')
+                    .append('ul')
+                    .selectAll('li')
+                    .data(item.items).enter()
+                    .append('li')
+                    .attr('class',function(item){
+                        return item.icon + ' tools-' + item.group;})
                     .on('click' , function(item) { 
-                    	if(item.items){return;}
-                    	if(item.mode){
-                        	context.enter(item.mode);	
-                    	} else if (item.action=='clipVisualExtent'){
-                    		//Call clip map
-                    		if(!_.isEmpty(hoot.model.layers.getLayers())){
-                    			hoot.control.utilities.clipdataset.clipDatasetContainer('visualExtent');                    			
-                    		} else {
-                    			iD.ui.Alert("Add data to map before clipping.","notice",new Error().stack);
-                    		}
-                    	} else if (item.action=='measureHelp'){
+                        if(item.items){return;}
+                        if(item.mode){
+                            context.enter(item.mode);   
+                        } else if (item.action=='clipVisualExtent'){
+                            //Call clip map
+                            if(!_.isEmpty(hoot.model.layers.getLayers())){
+                                hoot.control.utilities.clipdataset.clipDatasetContainer('visualExtent');                                
+                            } else {
+                                iD.ui.Alert("Add data to map before clipping.","notice",new Error().stack);
+                            }
+                        } else if (item.action=='measureHelp'){
                             iD.ui.Alert("Click anywhere on the map to start measuring.  Double-click to end measurement. Clicking on the Tools menu will clear the vectors from the screen.","notice");
                         }
-                    	d3.select('.tools-menu').remove();
-                    	d3.select('.sub-menu').remove();
-                      });
-            	
-            	subTools.append('span').attr("class",function(item){return item.icon + " icon icon-pre-text"});
-            	subTools.append('span').text(function(item) { return item.title; }).attr('title',(function(item){return item.tooltip||'';}));
+                        d3.select('.tools-menu').remove();
+                        d3.select('.sub-menu').remove();
+                      })
+                    .each(function(item){
+                        d3.select(this).call(iD.svg.Icon('#icon-' + item.icon, 'pre-text'));
+                    });
+
+                subTools.append('span').text(function(item) { return item.title; }).attr('title',(function(item){return item.tooltip||'';}));
             });
         
-            toolsItem.append('span').attr("class",function(item){return "icon-pre-text"});
             toolsItem.append('span').text(function(item) { return item.title; });
-           	
+            
         d3.select('.tools-menu').style('display', 'none');
         d3.select('.sub-menu').style('display', 'none');
         
         // show the context menu
         d3.select('.tools-menu')
-        	.style('left', function(){return d3.select('button.tools').property('offsetLeft')+'px'||'0px'})
+            .style('left', function(){return d3.select('button.tools').property('offsetLeft')+'px'||'0px'})
             .style('top', '120px')
             .style('display', 'block');
 
@@ -97,13 +98,13 @@ iD.ui.Tools = function(context) {
 
     return function(selection) {
         var button = selection.append('button')
-        	.attr('class', 'tools col12')
+            .attr('class', 'tools col12')
             .attr('tabindex', -1)
             .on('click', tools);
         
         button.append('span')
-	        .attr('class', 'label')
-	        .text(t('tools.title'));
+            .attr('class', 'label')
+            .text(t('tools.title'));
         
         var numChanges = 0;
     };
