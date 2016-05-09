8b1451bbb3bbe7f9c1b5d5565f844483bd94ccf4
sisskind <josh.sisskind@digitalglobe.com>
2016-05-09 10:29:43 -0400

    Corrected placement

--- js/hoot/ui/HootFormReviewmarkMenu.js
+++ js/hoot/ui/HootFormReviewmarkMenu.js
@@ -66,17 +66,26 @@ Hoot.ui.hootformreviewmarkmenu = function ()
     * @return returns created div.
     **/
     var _createFormDiv = function(container, containerId) {
+        /*
+        var left = d3.select('#' + containerId).node().offsetLeft;
+        var top = d3.select('#' + containerId).node().offsetTop;
+        var height = d3.select('#' + containerId).node().offsetHeight;
+        var width = d3.select('#' + containerId).node().offsetWidth;
+        var offsets = {'left':left,'top':top,'height':height,'width':width}
+        */
+
         var containerRect = d3.select('#' + containerId).node().getBoundingClientRect();
+        var containerSpan = d3.select('#' + containerId).select('span').node().getBoundingClientRect();
         var parentRect = d3.select('#utilReviewBookmarks').node().getBoundingClientRect();
 
-        var left = Math.ceil(containerRect.left - parentRect.left),
+        var left = Math.ceil(containerRect.left),
             top = Math.ceil(containerRect.top - parentRect.top),
             height = Math.ceil(containerRect.height),
             width = Math.ceil(containerRect.width);
         return container.append('div')
                 .classed('contain col3 row8 hoot-menu fill-white keyline-all round modal', true)
                 .style('top', (top + height) + 'px')
-                .style('left', (left + width) + 'px');
+                .style('left', (left - containerSpan.width) + 'px');
     };
 
     /**
