a02794370757e323c9134f78f3b02a10e5cb9fea
jcp2236 <joshpullin134@gmail.com>
2017-04-26 14:05:45 -0400

    Add in the core command detail modal window and styling consistent with other modals Hoot uses

--- js/id/ui/alert.js
+++ js/id/ui/alert.js
@@ -1,12 +1,14 @@
 iD.ui.Alert = function(message,type,stack) {
+    var modal = document.getElementById('myModal');
+    // var btn = document.getElementById("myBtn");
+    // var span = document.getElementsByClassName("close")[0];
     var alerts = d3.select('#alerts');
     var alertDiv = alerts.append('div')
         .classed('fillD alertDiv overflow',true)
         .on('click',function(){
             clearTimeout(uniqueTimeout);
             d3.select(this).transition().duration(0).style('opacity',1);
-        })
-        ;
+        });
 
     type = type.toLowerCase();
 
@@ -20,11 +22,30 @@ iD.ui.Alert = function(message,type,stack) {
     if(type === 'notice'){alertDiv.classed('blue',true);}
     if(type === 'success'){alertDiv.classed('green',true);}
 
+    function modalWindow() {
+        var modalbg = d3.select('body').append('div')
+            .attr('id','detailDiv')
+            .classed('fill-darken3 pin-top pin-left pin-bottom pin-right', true);
+        var detailModal = modalbg.append('div')
+            .classed('contain col8 pad1 hoot-menu fill-white round detailModal', true);
+        detailModal.append('h1')
+            .attr('id','processingDivLabel')
+            .style({'text-align': 'center'})
+            .text('Hoot Core Command Details');
+    }
+    window.onclick = function(event) {
+        if (event.target == d3.select('#detailDiv').node()) {
+            d3.select('#detailDiv').remove();
+        }
+    }
+
     alertDiv.append('div')
         .classed('fr _icon x dark point', true)
         .on('click',function() {this.parentNode.remove();});
 
     alertDiv.append('p').html(message);
+
+    alertDiv.append('a').on('click', modalWindow).attr('class', 'detailLink').text('More details');
 
     var d = new Date().toLocaleString();
     try{
