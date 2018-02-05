40a4d6d3b7f71f670fcc3a74643bfa3bc2a88652
Matt Putipong <mattputipong@gmail.com>
2018-02-05 17:58:41 -0500

    update dev server script to ES6

--- development_server.js
+++ development_server.js
@@ -1,90 +1,88 @@
 /* eslint-disable no-console */
 
-var gaze = require('gaze');
-var ecstatic = require('ecstatic');
-var colors = require('colors/safe');
-var express = require('express'),
-    app = express(),
-    url = require('url'),
-    proxy = require('express-http-proxy');
+let gaze     = require( 'gaze' ),
+	ecstatic = require( 'ecstatic' ),
+	colors   = require( 'colors/safe' ),
+	express  = require( 'express' ),
+	app      = express(),
+	url      = require( 'url' ),
+	proxy    = require( 'express-http-proxy' );
 
-var isDevelopment = process.argv[2] === 'develop';
+let isDevelopment = process.argv[ 2 ] === 'develop';
 
-var buildData = require('./build_data')(isDevelopment);
-var buildSrc = require('./build_src')(isDevelopment);
-var buildCSS = require('./build_css')(isDevelopment);
+let buildData = require( './build_data' )( isDevelopment ),
+	buildSrc  = require( './build_src' )( isDevelopment ),
+	buildCSS  = require( './build_css' )( isDevelopment );
 
-buildData()
-.then(function () {
-    return buildSrc();
-});
+buildData().then( () => {
+	return buildSrc();
+} );
 
 buildCSS();
 
-if (isDevelopment) {
-    gaze(['css/**/*.css'], function(err, watcher) {
-        watcher.on('all', function() {
-            buildCSS();
-        });
-    });
+if ( isDevelopment ) {
+	gaze( [ 'css/**/*.css' ], ( err, watcher ) => {
+		watcher.on( 'all', () => {
+			buildCSS();
+		} );
+	} );
 
-    gaze(
-        [
-            'data/**/*.{js,json}',
-            // ignore the output files of `buildData`
-            '!data/presets/categories.json',
-            '!data/presets/fields.json',
-            '!data/presets/presets.json',
-            '!data/presets.yaml',
-            '!data/taginfo.json',
-            '!dist/locales/en.json'
-        ],
-        function(err, watcher) {
-            watcher.on('all', function() {
-                buildData()
-                    .then(function () {
-                        // need to recompute js files when data changes
-                        buildSrc();
-                    });
-            });
-        }
-    );
+	gaze(
+		[
+			'data/**/*.{js,json}',
+			// ignore the output files of `buildData`
+			'!data/presets/categories.json',
+			'!data/presets/fields.json',
+			'!data/presets/presets.json',
+			'!data/presets.yaml',
+			'!data/taginfo.json',
+			'!dist/locales/en.json'
+		],
+		( err, watcher ) => {
+			watcher.on( 'all', () => {
+				buildData()
+					.then( () => {
+						// need to recompute js files when data changes
+						buildSrc();
+					} );
+			} );
+		}
+	);
 
-    gaze(['modules/**/*.js'], function(err, watcher) {
-        watcher.on('all', function() {
-            buildSrc();
-        });
-    });
+	gaze( [ 'modules/**/*.js' ], ( err, watcher ) => {
+		watcher.on( 'all', () => {
+			buildSrc();
+		} );
+	} );
 
-    var port = 8088;
-    var hootHost = 'localhost';
-    var hootPort = 8999;
-    var hootUrl = 'http://' + hootHost + ':' + hootPort;
+	let port     = 8088,
+		hootHost = 'localhost',
+		hootPort = 8999,
+		hootUrl  = 'http://' + hootHost + ':' + hootPort;
 
-    app.use(
-        ecstatic({ root: __dirname, cache: 0 })
-    ).listen(port);
+	app.use(
+		ecstatic( { root: __dirname, cache: 0 } )
+	).listen( port );
 
-    app.get('/hootenanny-id', function(req, res) {
-        res.writeHead(301,
-          { Location: 'http://localhost:8080' }
-        );
-        res.end();
-    });
+	app.get( '/hootenanny-id', ( req, res ) => {
+		res.writeHead( 301,
+			{ Location: 'http://localhost:8080' }
+		);
+		res.end();
+	} );
 
-    app.use('/hoot-services', proxy(hootUrl, {
-        forwardPath: function(req) {
-            return '/hoot-services' + url.parse(req.url).path;
-        }
-    }));
+	app.use( '/hoot-services', proxy( hootUrl, {
+		forwardPath: req => {
+			return '/hoot-services' + url.parse( req.url ).path;
+		}
+	} ) );
 
-    app.use('/static', proxy(hootUrl, {
-        forwardPath: function(req) {
-            return '/static' + url.parse(req.url).path;
-        }
-    }));
+	app.use( '/static', proxy( hootUrl, {
+		forwardPath: req => {
+			return '/static' + url.parse( req.url ).path;
+		}
+	} ) );
 
-    console.log(colors.yellow('Listening on ' + port));
-
+	console.log( colors.yellow( 'Listening on ' + port ) );
 
 }
