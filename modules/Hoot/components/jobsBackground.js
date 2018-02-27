50f54c1bc2f8b6985e31d26b5fa02b297bc0cee1
Matt Putipong <mattputipong@gmail.com>
2018-02-27 11:50:06 -0500

    #980 - added jobs background and settings panel

--- modules/Hoot/components/jobsBackground.js
+++ modules/Hoot/components/jobsBackground.js
@@ -7,19 +7,26 @@
 'use strict';
 
 class JobsBackground {
-	constructor( context ) {
-		this.$container = context.container();
-	}
+    constructor( context ) {
+        this.$container = context.container();
 
-	init() {
-		this.$jobsBG = this.$container
-			.append( 'div' )
-			.attr( 'id', 'jobsBG' )
-			.classed( 'col12 pin-bottom pin-top hidden', true )
-			.style( 'position', 'absolute' )
-			.style( 'top', '60px' )
-			.style( 'z-index', 999 )
-	}
+        this.defaultTab = null;
+    }
+
+    init() {
+        this.$jobsBG = this.$container
+            .append( 'div' )
+            .attr( 'id', 'jobsBG' )
+            .classed( 'col12 pin-bottom pin-top hidden', true )
+            .style( 'position', 'absolute' )
+            .style( 'top', '60px' )
+            .style( 'z-index', 999 );
+
+        this.$settingsSidebar = this.$jobsBG
+            .append( 'div' )
+            .attr( 'id', 'settingsSidebar' )
+            .classed( 'pad2 pin-bottom pin-top fill-light keyline-right', true );
+    }
 }
 
 export default JobsBackground;
