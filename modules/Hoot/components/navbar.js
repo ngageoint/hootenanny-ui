9fa3989fadb6bc1bafe2b88a299fbc189a2728de
Matt Putipong <mattputipong@gmail.com>
2018-03-01 12:36:11 -0500

    updated navbar

--- modules/Hoot/components/navbar.js
+++ modules/Hoot/components/navbar.js
@@ -20,12 +20,12 @@ export default class Header {
 
         let leftContainer = navbar
             .append( 'div' )
-            .classed( 'nav-left', true );
+            .classed( 'nav-item', true );
 
         // icon container
-        leftContainer
+        let settings = leftContainer
             .append( 'div' )
-            .classed( 'vertical-align icon-container', true )
+            .classed( 'pointer icon-container flex justify-between', true )
             .on( 'click', function() {
                 d3.event.stopPropagation();
                 d3.event.preventDefault();
@@ -40,18 +40,45 @@ export default class Header {
                     .classed( 'hidden', vis );
 
                 d3.selectAll( '.context-menu, .tools-menu, .dataset-options-menu' ).remove();
-            } )
+            } );
+
+        settings
             .append( 'i' )
-            .classed( 'pointer medium material-icons', true )
+            .classed( 'medium material-icons', true )
             .text( 'settings' );
 
-        // logo container
-        leftContainer
+        settings
+            .append( 'span' )
+            .text( 'Manage' );
+
+        //// logo container
+        //leftContainer
+        //    .append( 'div' )
+        //    .classed( 'logo-container', true )
+        //    .append( 'img' )
+        //    .attr( 'src', './img/hoot_logo_update.png' )
+        //    .classed( 'pointer hoot-logo', true );
+
+        let rightContainer = navbar
+            .append( 'div' )
+            .classed( 'nav-item', true );
+
+        rightContainer
             .append( 'div' )
             .classed( 'logo-container', true )
             .append( 'img' )
             .attr( 'src', './img/hoot_logo_update.png' )
             .classed( 'pointer hoot-logo', true );
+
+        rightContainer
+            .append( 'div' )
+            .classed( 'vertical-align icon-container', true )
+            .on( 'click', function() {
+
+            } )
+            .append( 'i' )
+            .classed( 'pointer medium material-icons', true )
+            .text( 'menu' );
     }
 
     init() {
