11c6e24690bed38e0df44b9ba71e7d41542f9eac
Matt Putipong <mattputipong@gmail.com>
2018-05-08 22:00:56 -0400

    production build optimization

--- modules/Hoot/hoot.js
+++ modules/Hoot/hoot.js
@@ -13,7 +13,7 @@ import SettingsPanel from './settingsPanel';
 /**
  * Entry point for Hoot UI
  */
-class Hoot {
+export default class Hoot {
     constructor( context ) {
         this.context   = context;
         this.container = context.container();
@@ -23,10 +23,10 @@ class Hoot {
         LayerManager.ctx = this.context;
         HootOSM.ctx      = this.context;
 
-        new Navbar( d3.select( '#id-container' ), this.context ).render();
-        new SettingsPanel( this.container, this.context ).render();
-        new Sidebar( d3.select( '#sidebar' ), this.context ).render();
+        Promise.all( [
+            new Navbar( d3.select( '#id-container' ), this.context ).render(),
+            new SettingsPanel( this.container, this.context ).render(),
+            new Sidebar( d3.select( '#sidebar' ), this.context ).render()
+        ] );
     }
 }
-
-export default Hoot;
