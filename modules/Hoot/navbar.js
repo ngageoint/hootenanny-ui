99d640e6343f701a632f9b4b3ab9ea63f59806eb
Matt Putipong <mattputipong@gmail.com>
2018-07-11 15:45:11 -0400

    add dropdown to right side of navbar

--- modules/Hoot/navbar.js
+++ modules/Hoot/navbar.js
@@ -13,47 +13,38 @@
 export default class Navbar {
     constructor( container ) {
         this.container = container;
+
+        this.dropdownItems = [
+            {
+                title: 'About',
+                onClick: () => this.openAboutModal()
+            }
+        ];
     }
 
     /**
      * Render navbar and all of its view
      */
     async render() {
-        this.createNavbar();
-        this.createLeftContainer();
-        this.createMenuButton();
-        this.createLogo();
-
-        return this;
-    }
-
-    /**
-     * Create navbar container
-     *
-     * @returns {d3} - navbar container
-     */
-    createNavbar() {
         this.navbar = this.container
             .insert( 'nav', ':first-child' )
             .attr( 'id', 'navbar' )
             .classed( 'contain text-white fill-dark', true );
+
+        this.createLeftContainer();
+        this.createRightContainer();
+
+        return this;
     }
 
-    /**
-     * Create left-aligned container
-     */
     createLeftContainer() {
-        this.leftContainer = this.navbar
+        let leftContainer = this.navbar
             .append( 'div' )
             .classed( 'nav-item', true );
-    }
 
-    /**
-     * Create a menu button in the left container
-     */
-    createMenuButton() {
-        this.menuButton = this.leftContainer.append( 'div' )
-            .classed( 'button dark text-light pointer icon-container flex justify-between', true )
+        let menuButton = leftContainer
+            .append( 'div' )
+            .classed( 'button dark text-light pointer icon-container', true )
             .on( 'click', function() {
                 d3.event.stopPropagation();
                 d3.event.preventDefault();
@@ -70,20 +61,67 @@ export default class Navbar {
                 d3.selectAll( '.context-menu, .tools-menu, .dataset-options-menu' ).remove();
             } );
 
-        this.menuButton.append( 'i' )
+        menuButton
+            .append( 'i' )
             .classed( 'medium material-icons', true )
             .text( 'menu' );
-    }
 
-    /**
-     * Create the Hoot logo in the left container
-     */
-    createLogo() {
-        this.leftContainer
+        leftContainer
             .append( 'div' )
             .classed( 'logo-container', true )
             .append( 'img' )
             .attr( 'src', './img/hoot_logo_update.png' )
             .classed( 'pointer hoot-logo', true );
+    }
+
+    createRightContainer() {
+        let rightContainer = this.navbar
+            .append( 'div' )
+            .classed( 'nav-item', true );
+
+        let dropdownContent = rightContainer
+            .append( 'ul' )
+            .classed( 'dropdown-content fill-white', true )
+            .selectAll( '.dropdown-item' )
+            .data( this.dropdownItems );
+
+        dropdownContent
+            .enter()
+            .append( 'li' )
+            .classed( 'dropdown-item pad2x strong pointer', true )
+            .append( 'a' )
+            .attr( 'href', '#!' )
+            .text( d => d.title );
+
+        let dropdownToggle = rightContainer
+            .append( 'div' )
+            .classed( 'flex align-center text-light pointer', true )
+            .on( 'click', () => this.toggleDropdown() );
+
+        dropdownToggle
+            .append( 'i' )
+            .classed( 'medium material-icons', true )
+            .text( 'info_outline' );
+
+        dropdownToggle
+            .append( 'i' )
+            .classed( 'medium material-icons', true )
+            .text( 'arrow_drop_down' );
+    }
+
+    toggleDropdown() {
+        let dropdown = d3.select( '.dropdown-content' ),
+            visibleState = dropdown.classed( 'visible' );
+
+        dropdown.classed( 'visible', !visibleState );
+
+        if ( !visibleState ) {
+            d3.select( 'body' )
+                .on( 'mousedown', () => dropdown.classed( 'visible', false ) );
+        }
+    }
+
+    openAboutModal() {
+
     }
 }
