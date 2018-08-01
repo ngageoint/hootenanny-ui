ad09b5c09a6663bb0ac88b27c0f6f21614e97428
Matt Putipong <mattputipong@gmail.com>
2018-08-01 11:22:24 -0400

    split code for clip modals into separate files
    resolves #1186

--- modules/Hoot/tools/clipSelectBbox.js
+++ modules/Hoot/tools/clipSelectBbox.js
@@ -4,22 +4,17 @@
  * @author Matt Putipong - matt.putipong@radiantsolutions.com on 7/25/18
  *******************************************************************************************************/
 
-import _             from 'lodash-es';
-import EventEmitter  from 'events';
-import FormFactory   from './formFactory';
-import LayerManager  from '../managers/layerManager';
-import FolderManager from '../managers/folderManager';
+import EventEmitter from 'events';
+import ClipDataset  from './clipDataset';
+import FormFactory  from './formFactory';
 
-import { checkForUnallowedChar } from './utilities';
-import { modeClipBoundingBox }   from '../../modes';
-import { d3combobox }            from '../../lib/hoot/d3.combobox';
+import { modeClipBoundingBox } from '../../modes';
 
 export default class ClipSelectBbox extends EventEmitter {
     constructor( context ) {
         super();
 
-        this.context     = context;
-        this.formFactory = new FormFactory();
+        this.context = context;
 
         this.minlon   = null;
         this.minlat   = null;
@@ -34,11 +29,11 @@ export default class ClipSelectBbox extends EventEmitter {
             button: {
                 text: 'Next',
                 id: 'clipNextBtn',
-                onClick: () => this.renderClipDataset( this.clipType )
+                onClick: () => this.handleNext()
             }
         };
 
-        this.container  = this.formFactory.generateForm( 'body', 'selectClipBbox', metadata );
+        this.container  = new FormFactory().generateForm( 'body', 'selectClipBbox', metadata );
         this.form       = d3.select( '#selectClipBbox' );
         this.nextButton = d3.select( '#clipNextBtn' );
 
@@ -167,188 +162,10 @@ export default class ClipSelectBbox extends EventEmitter {
         }, 100 );
     }
 
-    renderClipDataset() {
-        let titleText = this.clipType === 'visualExtent'
-            ? 'Clip Data to Visual Extent'
-            : this.clipType === 'boundingBox'
-                ? 'Clip Data to Bounding Box'
-                : 'Clip Data';
-
-        let metadata = {
-            title: titleText,
-            button: {
-                text: 'Clip',
-                id: 'clipSubmitBtn',
-                onClick: this.handleSubmit()
-            }
-        };
-
+    handleNext() {
         this.container.remove();
         this.nextButton = null;
 
-        this.container    = this.formFactory.generateForm( 'body', 'clipDataset', metadata );
-        this.form         = d3.select( '#clipDataset' );
-        this.submitButton = d3.select( '#clipSubmitBtn' );
-
-        this.submitButton.property( 'disabled', false );
-
-        this.createTable();
-    }
-
-    createTable() {
-        let loadedLayers = LayerManager.loadedLayers,
-            folderList   = FolderManager.folderPaths,
-            that         = this;
-
-        let columns = [
-            {
-                label: 'Dataset',
-                type: 'datasetName'
-            },
-            {
-                label: 'Clip?',
-                checkbox: true
-            },
-            {
-                label: 'Output Name',
-                placeholder: 'Save As',
-                type: 'layerName'
-            },
-            {
-                label: 'Path',
-                placeholder: 'root',
-                combobox: folderList
-            }
-        ];
-
-        let table = this.form
-            .insert( 'table', '.modal-footer' )
-            .attr( 'id', 'clipTable' );
-
-        let colgroup = table
-            .append( 'colgroup' );
-
-        colgroup.append( 'col' )
-            .attr( 'span', '1' );
-
-        colgroup.append( 'col' )
-            .style( 'width', '100px' );
-
-        table
-            .append( 'thead' )
-            .append( 'tr' )
-            .selectAll( 'th' )
-            .data( columns )
-            .enter()
-            .append( 'th' )
-            .text( d => d.label );
-
-        let tableBody = table.append( 'tbody' );
-
-        _.forEach( loadedLayers, layer => {
-            let mapId = layer.id;
-
-            tableBody
-                .append( 'tr' )
-                .attr( 'id', `row-${ mapId }` )
-                .selectAll( 'td' )
-                .data( columns )
-                .enter()
-                .append( 'td' )
-                .append( 'input' )
-                .attr( 'type', 'text' )
-                .attr( 'placeholder', d => d.placeholder )
-                .select( function( d ) {
-                    if ( d.checkbox ) {
-                        let parent = d3.select( this.parentElement );
-
-                        parent
-                            .selectAll( 'input' )
-                            .remove();
-
-                        parent
-                            .append( 'input' )
-                            .attr( 'type', 'checkbox' )
-                            .property( 'checked', true )
-                            .attr( 'id', `clip-${ mapId }` );
-                    }
-
-                    if ( d.combobox ) {
-                        that.createFolderListCombo( d3.select( this ), d, layer );
-                    }
-
-                    if ( d.type === 'datasetName' ) {
-                        d3.select( this )
-                            .attr( 'placeholder', layer.name )
-                            .attr( 'readonly', true );
-                    }
-
-                    if ( d.type === 'layerName' ) {
-                        that.createLayerNameField( d3.select( this ), layer );
-                    }
-                } );
-        } );
-    }
-
-    createLayerNameField( input, layer ) {
-        let that       = this,
-            uniquename = false,
-            layerName  = layer.name,
-            i          = 1;
-
-        while ( uniquename === false ) {
-            if ( !_.isEmpty( LayerManager.findBy( 'name', layerName ) ) ) {
-                layerName = layer.name + i.toString();
-                i++;
-            } else {
-                uniquename = true;
-            }
-        }
-
-        input
-            .property( 'value', layerName )
-            .on( 'input', function() {
-                let resp = checkForUnallowedChar( this.value );
-
-                if ( resp !== true ) {
-                    d3.select( this ).classed( 'invalid', true ).attr( 'title', resp );
-                    that.submitButton.property( 'disabled', true );
-                } else {
-                    d3.select( this ).classed( 'invalid', false ).attr( 'title', null );
-                    that.submitButton.property( 'disabled', false );
-                }
-            } );
-    }
-
-    /**
-     * Create folder list selection dropdown
-     *
-     * @param input - selected field
-     * @param d     - selected field meta-data
-     * @param layer - selected layer meta-data
-     **/
-    createFolderListCombo( input, d, layer ) {
-        let combobox = d3combobox()
-            .data( _.map( d.combobox, n => {
-                return {
-                    value: n.path,
-                    title: n.path
-                };
-            } ) );
-
-        let data = combobox.data();
-
-        data.sort( ( a, b ) => {
-            let textA = a.value.toLowerCase(),
-                textB = b.value.toLowerCase();
-
-            return textA < textB ? -1 : textA > textB ? 1 : 0;
-        } ).unshift( { value: 'root', title: 0 } );
-
-        input.call( combobox );
-    }
-
-    handleSubmit() {
-
+        new ClipDataset( this ).render( this.clipType );
     }
 }
