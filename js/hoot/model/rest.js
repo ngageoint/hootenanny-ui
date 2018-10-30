38eeaeaaa3b53970710a7141d6305b7618cc2b61
Arni Sumarlidason <sumarlidason@gmail.com>
2018-10-30 11:04:02 -0400

    update user URIs
    refs #1248
    
    * update user URIs

--- js/hoot/model/rest.js
+++ js/hoot/model/rest.js
@@ -759,7 +759,7 @@ Hoot.model.REST = function (command, data, callback, option) {
 
 
     rest.getSaveUser = function(data, callback) {
-        d3.json('/hoot-services/osm/user/-1?userEmail=' + data.email)
+        d3.json('/hoot-services/osm/user?userEmail=' + data.email)
             .header('Content-Type', 'application/json')
             .post(JSON.stringify(data), function (error, resp) {
 
@@ -771,7 +771,7 @@ Hoot.model.REST = function (command, data, callback, option) {
     };
 
     rest.getAllUsers = function(callback) {
-        d3.json('/hoot-services/osm/user/-1/all', function (error, resp) {
+        d3.json('/hoot-services/osm/user/all', function (error, resp) {
             if (error) {
                 return callback(_alertError(error, 'Get all users failed!'));
             }
