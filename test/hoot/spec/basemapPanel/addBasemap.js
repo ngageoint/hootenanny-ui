5db62618c6fa1cf4870face9e0d345d695cfccc7
Jack Grossman <jack.grossman@radiantsolutions.com>
2018-11-26 15:32:49 -0500

    Updated d3 pathways

--- test/hoot/spec/basemapPanel/addBasemap.js
+++ test/hoot/spec/basemapPanel/addBasemap.js
@@ -1,27 +1,30 @@
 /** ****************************************************************************************************
  * File: addBasemap.js
  * Project: hootenanny-ui
- * @author Jack Grossman on 11/14/18 jack.grossman@radiantsolutions.com
+ * @author Jack Grossman on 11/26/18 jack.grossman@radiantsolutions.com
  *******************************************************************************************************/
 describe( 'Basemap component rendered', () => {
     it( ' Activates basemap selector ', done => {
-        d3.select('nav#navbar div.nav-item div.menu-button').dispatch('click');
-      d3.select('#manage-sidebar-menu div.tab-header:nth-child(3)').dispatch('click');
-      setTimeout(() => {
-        var selectBasemap = d3.selectAll('#util-basemaps').attr('id');
-        expect(selectBasemap).to.be.eql('util-basemaps');
-        done();
-      }, 1000);
+        d3.select('div.menu-button').dispatch('click');
+        var basemapNode = d3.select('#manage-sidebar-menu div.tab-header:nth-child(3)')
+        basemapNode.dispatch('click');
+        setTimeout(() => {
+            var selectBasemap = d3.selectAll('#util-basemaps').attr('id');
+            expect(selectBasemap).to.be.eql('util-basemaps');
+            done();
+        }, 1000);
     } );
     it( 'Raster input window opens', done => {
-        d3.select('#util-basemaps  div  button').dispatch('click');      
+        d3.select('.add-basemap-button').dispatch('click');      
         setTimeout(() => {
-            expect(d3.select('#basemaps-add-form > div > div').size() ).to.equal( 1 );
+            var newBasemapWindow = d3.select('.contain hoot-menu');
+            expect(d3.select(newBasemapWindow).size() ).to.equal( 1 );
             done();
         }, 2000);
     } );
     it( 'Raster input window closes', done => {
-        d3.select('#basemaps-add-form  div  div  form  div  h3  div').dispatch('click');
+        var windowClose = d3.select('.fr _icon close pointer');
+        windowClose.dispatch('click');
 
         setTimeout(() => {
             expect(d3.select('#basemaps-add-form  div  div  form  div  h3  div').size() ).to.equal( 0 );
@@ -29,14 +32,16 @@ describe( 'Basemap component rendered', () => {
         }, 1000);
     } );
     it( 'File added to table', done => {
+        var newBasemapWindow = d3.select('.contain hoot-menu');
         setTimeout(() => {
-            expect(d3.select('#util-basemaps  div  div').size() ).to.equal( 1 );
+            expect(newBasemapWindow.size() ).to.equal( 1 );
             done();
         },1000);
     }); 
     it( 'Basemap table contains buttons', done => {
         setTimeout(() => {
-            expect(d3.select('#util-basemaps  div  div  div  div').size() ).to.equal( 1 );
+            var tableButtons = d3.select('.button-container');
+            expect(tableButtons.size() ).to.be.equal( 1 );
             done();
         }, 1000);
     } );
