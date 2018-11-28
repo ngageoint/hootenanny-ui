ee6ee5e3bb96cf2cf1a7a6efed4ad19cfc2ca041
Matt Putipong <mattputipong@gmail.com>
2018-11-28 11:15:11 -0500

    update remaining CSS URLs to use relative paths

--- webpack-config/webpack.base.config.js
+++ webpack-config/webpack.base.config.js
@@ -30,7 +30,7 @@ module.exports = {
     output: {
         path: resolve( __dirname, '../dist/' ),
         filename: '[name].min.js',
-        publicPath: '/'
+        publicPath: './'
     },
     module: {
         rules: [
