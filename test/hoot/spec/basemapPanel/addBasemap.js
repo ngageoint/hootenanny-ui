f1baad617aa168bb01a5d78df4910869b5d1a52b
Jack Grossman <jack.grossman@radiantsolutions.com>
2018-12-17 12:25:53 -0500

    Updated basemap test to include async deletion of raster file

---
+++ test/hoot/spec/basemapPanel/addBasemap.js
@@ -0,0 +1,64 @@
+/** ****************************************************************************************************
+ * File: addBasemap.js
+ * Project: hootenanny-ui
+ * @author Jack Grossman on 11/26/18 jack.grossman@radiantsolutions.com
+ *******************************************************************************************************/
+
+const { retrieveFile } = require( '../../helpers' );
+
+describe( 'Basemap component rendered', () => {
+
+
+    before( async function() { 
+
+        this.timeout( 10000 );
+
+        try {
+            
+            d3.select( '.add-basemap-button' ).dispatch( 'click' );
+
+
+            let rasterImport = Hoot.ui.managePanel.basemaps.addBasemapModal;
+            let fileIngest = rasterImport.fileIngest;
+            let dT   = new ClipboardEvent( '' ).clipboardData || new DataTransfer(),
+            file = await retrieveFile( 'base/test/data/RomanColosseum_WV2naturalcolor_clip.tif' );
+            
+            dT.items.add( file );
+
+            fileIngest.node().files = dT.files;
+
+            await fileIngest.dispatch( 'change' );
+
+            var rasterName = d3.select( '#basemapName' );
+
+            rasterName.property( 'value',  'UnitTestImportBasemap' )
+                .dispatch( 'keyup' );
+
+            d3.select( '#basemapAddBtn' ).dispatch( 'click' );
+          
+        } catch (e) {
+
+             JSON.stringify( e );
+        }
+
+    } );
+    
+    it( 'New basemap added to basemap table', done => {
+        setTimeout( () => {
+            var newBasemap = d3.select( '#util-basemaps span' );
+            expect(newBasemap.text() ).to.be.eql( 'UnitTestImportBasemap' );
+            done(); 
+        }, 5000 );
+
+    } );
+
+    after( async () => {
+        var basemaps = await Hoot.api.getBasemaps();
+        if ( basemaps.findIndex( function(d) { return d.name === 'UnitTestImportBasemap'; } ) > -1 ) {
+            console.log( 'Deleting basemap: "UnitTestImportBasemap"');
+            await Hoot.api.deleteBasemap('UnitTestImportBasemap')
+                .then( () => Hoot.ui.managePanel.basemaps.loadBasemaps());
+        }
+    } );   
+
+  } );
