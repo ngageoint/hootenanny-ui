6f48df388c3778c98a423223648967949c0268f1
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2019-02-14 18:30:43 -0500

    strip any filename at end of path
    and make relative to ../ unless we are at the root
    
    refs #1307

--- modules/Hoot/config/apiConfig.js
+++ modules/Hoot/config/apiConfig.js
@@ -5,10 +5,21 @@
  * @author Matt Putipong on 3/2/18
  *******************************************************************************************************/
 
+// strip any filename at end of path and make relative to ../
+// unless we are at the root
+
+function relativePath() {
+    let path = window.location.pathname;
+    let pathWithoutFile = path.substr(0, path.lastIndexOf("/"));
+    let pathRelative = ( (pathWithoutFile === '') ? '' : (pathWithoutFile + '..')) + '/hoot-services';
+
+    return pathRelative;
+}
+
 export const apiConfig = {
     host: window.location.protocol + '//' + window.location.host, // just host name without port
     port: window.location.port,
-    path: window.location.pathname + '../hoot-services',
+    path: relativePath(),
     translationServerPort: '8094',
     mergeServerPort: '8096',
     queryInterval: 2000
