b4c0c3a1827be71f08ab17bef8aeb1fdda7f6f6d
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2019-02-25 20:22:56 -0500

    relative path should just be '/' or '../' (#1357)
    * relative path should just be '/' or '../'
    
    * this is needed, add back in and later find out why

--- modules/Hoot/config/apiConfig.js
+++ modules/Hoot/config/apiConfig.js
@@ -11,7 +11,7 @@
 function relativePath() {
     let path = window.location.pathname;
     let pathWithoutFile = path.substr(0, path.lastIndexOf('/'));
-    let pathRelative = ( (pathWithoutFile === '') ? '' : (pathWithoutFile + '..')) + '/hoot-services';
+    let pathRelative = ( (pathWithoutFile === '') ? '' : '..' ) + '/hoot-services';
 
     return pathRelative;
 }
