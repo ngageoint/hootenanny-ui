76475b31355bc4b60063cd506f0c277f1d5ac64e
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2019-03-08 17:37:50 -0500

    bind this and remove unnecessary code

--- modules/Hoot/ui/managePanel/jobs.js
+++ modules/Hoot/ui/managePanel/jobs.js
@@ -26,7 +26,7 @@ export default class Jobs extends Tab {
     }
 
     activate() {
-        this.poller = window.setInterval( this.loadJobs, 5000 );
+        this.poller = window.setInterval( this.loadJobs.bind(this), 5000 );
     }
 
     deactivate() {
@@ -51,7 +51,6 @@ export default class Jobs extends Tab {
     }
 
     populateJobs( jobs ) {
-        let instance = this;
 
         let rows = this.jobsTable
             .selectAll( '.jobs-item' )
@@ -70,73 +69,6 @@ export default class Jobs extends Tab {
             .append( 'span' )
             .text( d => d.jobId );
 
-        // let buttonContainer = basemapItem
-        //     .append( 'div' )
-        //     .classed( 'button-container fr', true );
-
-        // buttonContainer
-        //     .append( 'button' )
-        //     .classed( 'keyline-left _icon', true )
-        //     .on( 'click', function( d ) {
-        //         let button = d3.select( this );
-
-        //         d3.event.stopPropagation();
-        //         d3.event.preventDefault();
-
-        //         if ( d.status === 'disabled' ) {
-        //             Hoot.api.enableBasemap( d ).then( () => {
-        //                 button
-        //                     .classed( 'closedeye', false )
-        //                     .classed( 'openeye', true );
-
-        //                 d.status = 'enabled';
-
-        //                 instance.renderBasemap( d );
-        //             } );
-        //         } else {
-        //             Hoot.api.disableBasemap( d ).then( () => {
-        //                 button
-        //                     .classed( 'closedeye', true )
-        //                     .classed( 'openeye', false );
-
-        //                 d.status = 'disabled';
-
-        //                 Hoot.context.background().removeBackgroundSource( d );
-        //             } );
-        //         }
-        //     } )
-        //     .select( function( d ) {
-        //         let button = d3.select( this );
-
-        //         if ( d.status === 'processing' ) {
-        //             //TODO: get back to this
-        //             window.console.log( 'processing' );
-        //         } else if ( d.status === 'failed' ) {
-        //             window.console.log( 'failed' );
-        //         } else if ( d.status === 'disabled' ) {
-        //             button.classed( 'closedeye', true );
-        //             button.classed( 'openeye', false );
-        //         } else {
-        //             button.classed( 'closedeye', false );
-        //             button.classed( 'openeye', true );
-        //             // basemaps is already enabled, so just render it in the UI
-        //             instance.renderBasemap( d );
-        //         }
-        //     } );
-
-        // buttonContainer
-        //     .append( 'button' )
-        //     .classed( 'keyline-left _icon trash', true )
-        //     .on( 'click', function( d ) {
-        //         d3.event.stopPropagation();
-        //         d3.event.preventDefault();
-
-        //         let r = confirm( `Are you sure you want to delete: ${ d.name }?` );
-        //         if ( !r ) return;
-
-        //         Hoot.api.deleteBasemap( d.name )
-        //             .then( () => instance.loadBasemaps() );
-        //     } );
     }
 
 }
