f28843dafb52f2bbadac66d6d946fb4b57c06c33
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2019-03-12 15:09:19 -0400

    checkpoint: probably not going to use a table

--- modules/Hoot/ui/managePanel/jobs.js
+++ modules/Hoot/ui/managePanel/jobs.js
@@ -50,24 +50,84 @@ export default class Jobs extends Tab {
         }
     }
 
+/*
+{
+"jobId": "bb266d64-5251-4c67-a2ca-347f26892055",
+"jobType": "delete",
+"userId": 2631622,
+"mapId": 8,
+"start": 1552411634022,
+"end": 1552411634144,
+"status": "complete",
+"percentcomplete": 100
+}
+*/
     populateJobs( jobs ) {
+        let table = this.jobsTable
+            .selectAll('table')
+            .data([0]);
+        table = table.enter()
+                .append('table')
+                .merge(table);
 
-        let rows = this.jobsTable
-            .selectAll( '.jobs-item' )
-            .data( jobs, d => d.name );
+        table.selectAll('tr.head')
+            .data([0])
+            .enter().append('tr')
+            .classed('head', true)
+            .selectAll('th')
+            .data([
+                'Job Type',
+                'Output',
+                'Owner',
+                'Status',
+                'Started',
+                'Duration',
+                'Percent Complete',
+                // 'Actions'
+                ])
+            .enter().append('th')
+            .text(d => d);
+
+        let rows = table
+            .selectAll( 'tr.jobs-item' )
+            .data( jobs, d => d.jobId );
 
         rows.exit().remove();
 
-        let jobItem = rows
+        rows = rows
             .enter()
-            .append( 'div' )
-            .classed( 'jobs-item keyline-bottom', true );
+            .append( 'tr' )
+            .classed( 'jobs-item keyline-bottom', true )
+            .merge(rows);
 
-        rows = rows.merge( jobItem );
+        let cells = rows.selectAll( 'td' )
+            .data(d => {
+                let props = [];
 
-        jobItem
-            .append( 'span' )
-            .text( d => d.jobId );
+                let map = Hoot.layers.findBy( 'id', d.mapId );
+
+                props.push(d.jobType);
+                props.push(map ? map.name : 'Unknown');
+                props.push(
+                    (Hoot.config.users[ d.userId ]) ?
+                        Hoot.config.users[ d.userId ].display_name :
+                        'No user for ' + d.userId);
+                props.push(d.status);
+                //props.push(moment( d.start ).fromNow());
+                //props.push(moment.duration( ((d.end) ? d.end : )
+                props.push(d.start);
+                props.push(d.end);
+                props.push(d.percentcomplete);
+
+                return props;
+            });
+
+        cells.exit().remove();
+
+        cells = cells
+            .enter().append( 'td' )
+            .merge(cells);
+        cells.text( d => d );
 
     }
 
