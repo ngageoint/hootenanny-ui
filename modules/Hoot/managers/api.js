cd5dec7484e98dcc69cdda4791aa5ded4b49cc7f
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2019-03-23 16:27:32 -0400

    remove mock data

--- modules/Hoot/managers/api.js
+++ modules/Hoot/managers/api.js
@@ -225,31 +225,13 @@ export default class API {
     }
 
     getJobsRunning() {
-        // const params = {
-        //     path: '/jobs/running',
-        //     method: 'GET'
-        // };
+        const params = {
+            path: '/jobs/running',
+            method: 'GET'
+        };
 
-        // return this.request( params )
-        //     .then( resp => resp.data );
-
-let running = [
-{
-"jobId": "ex_1a939fbecd51424aa00ddad4399500d1",
-"jobType": "export",
-"start": moment().subtract(5, 'minutes'),
-"percentcomplete": 33.3,
-"userId": 8964869
-},
-{
-"jobId": "592110bd-5670-41ad-addb-8b6cec812abd",
-"jobType": "conflate",
-"start": moment().subtract(1, 'hour'),
-"percentcomplete": 58.7,
-"userId": 2631622
-}
-];
-       return running;
+        return this.request( params )
+            .then( resp => resp.data );
     }
 
     getCoreVersionInfo() {
