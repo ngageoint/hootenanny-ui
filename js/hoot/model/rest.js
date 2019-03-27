809c157ae10061adcc023204d8f943b3185f36bd
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2019-03-27 14:28:29 -0400

    let's use isNaN

--- js/hoot/model/rest.js
+++ js/hoot/model/rest.js
@@ -270,7 +270,7 @@ Hoot.model.REST = function (command, data, callback, option) {
     };
 
     rest.clipDataset = function (context, data, callback) {
-        if(!data.INPUT_NAME || !data.BBOX || !data.OUTPUT_NAME){return false;}
+        if(!data.INPUT_NAME || !data.BBOX || !data.OUTPUT_NAME || isNaN(data.FOLDER_ID)){return false;}
 
         var postClip = function(a) {
             if(a.status==='complete'){
