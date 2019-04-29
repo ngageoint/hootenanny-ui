12e430350015853143695d7c4e7fd3e687babfec
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2019-04-29 16:31:30 -0400

    update fields names for folder properties

--- modules/Hoot/config/domMetadata.js
+++ modules/Hoot/config/domMetadata.js
@@ -243,14 +243,14 @@ export function importMultiForm() {
 export function addFolderForm() {
     return [
         {
-            label: 'Folder Name',
+            label: 'Name',
             id: 'addFolderName',
             class: 'new-folder-name',
             inputType: 'text',
             onChange: d => this.validateTextInput( d )
         },
         {
-            label: 'Folder Visibility',
+            label: 'Public',
             id: 'addFolderVisibility',
             inputType: 'checkbox',
             value: 'Public',
@@ -289,7 +289,7 @@ export function modifyDatasetForm() {
             onChange: d => this.validateTextInput( d )
         },
         {
-            label: 'Visibility',
+            label: 'Public',
             id: 'modifyVisibility',
             inputType: 'checkbox',
             value: 'Public',
