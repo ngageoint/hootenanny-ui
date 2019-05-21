73bce2caf39f875362a45c9b9db5b84d18d2235b
Max Grossman <m.grossman1813@gmail.com>
2019-05-21 13:53:25 -0400

    add bundle analyzer build... ref #1432

---
+++ webpack-config/webpack.analyze.config.js
@@ -0,0 +1,19 @@
+/** ****************************************************************************************************
+ * File: webpack.dev.config.js
+ * Project: hootenanny-ui
+ * @author Matt Putipong on 10/24/18
+ *******************************************************************************************************/
+
+const Merge        = require( 'webpack-merge' );
+const CommonConfig = require('./webpack.base.config');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = Merge( CommonConfig, {
+    plugins: [
+        new BundleAnalyzerPlugin({ // if you want to json, just uncomment
+            // analyzerMode: 'disabled',
+            // generateStatsFile: true,
+            // statsFilename: 'bundle_stats.json'
+        })
+    ]
+} );
