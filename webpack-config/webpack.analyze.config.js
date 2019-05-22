64c7ba38d0e4c409d7ed57a9dbe9d7e4f5ea5c96
Max Grossman <m.grossman1813@gmail.com>
2019-05-22 10:01:42 -0400

    just use fetch api...
    ref #1432

--- webpack-config/webpack.analyze.config.js
+++ webpack-config/webpack.analyze.config.js
@@ -11,6 +11,7 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 module.exports = Merge( CommonConfig, {
     plugins: [
         new BundleAnalyzerPlugin({ // if you want to json, just uncomment
+            analyzerPort: '8675',
             // analyzerMode: 'disabled',
             // generateStatsFile: true,
             // statsFilename: 'bundle_stats.json'
