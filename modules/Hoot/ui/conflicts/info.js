2880687adad70d2853782ccdae58e1539a3755e2
Jack Grossman <jack.grossman@radiantsolutions.com>
2019-07-02 11:22:32 -0400

    updated panToEntity, click on tag-table maintains consistent zoom level

--- modules/Hoot/ui/conflicts/info.js
+++ modules/Hoot/ui/conflicts/info.js
@@ -119,11 +119,11 @@ export default class ConflictMetadata {
 
     panToEntity(feature) {
         let extent = feature.extent(Hoot.context.graph());
-        if (feature.type === 'node') {
+        //if (feature.type === 'node') {
             Hoot.context.map().centerZoom(extent.center(), 21);
-        } else {
-            Hoot.context.map().centerZoom(extent.center(), Hoot.context.map().trimmedExtentZoom(extent) - 0.5);
-        }
+        //} else {
+            //Hoot.context.map().centerZoom(extent.center(), Hoot.context.map().trimmedExtentZoom(extent) - 0.5);
+        //}
     }
 
     /**
