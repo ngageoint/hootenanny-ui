e7023ecbc5e84912853f835cb66d5eab56b2d9b3
Jack Grossman <jack.grossman@radiantsolutions.com>
2019-11-22 16:13:03 -0500

    updating boolean for filterVisible

--- modules/Hoot/managers/folderManager.js
+++ modules/Hoot/managers/folderManager.js
@@ -79,7 +79,7 @@ export default class FolderManager {
     }
 
     filterVisible(f) {
-        return JSON.parse(Hoot.context.storage( 'publicVisibility' )) && f.id !== 0 || f.userId === Hoot.user().id && f.id !== 0;
+        return f.id !== 0  && (JSON.parse(Hoot.context.storage( 'publicVisibility' )) ||  f.userId === Hoot.user().id);
     }
 
     /**
