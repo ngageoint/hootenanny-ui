4241bd5daa478fbf29f555a9629b91f8cbb40325
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2020-02-13 15:12:06 -0500

    add another num pages condition

--- modules/Hoot/ui/managePanel/jobs.js
+++ modules/Hoot/ui/managePanel/jobs.js
@@ -364,7 +364,10 @@ export default class Jobs extends Tab {
 
     populateJobsHistory( jobs ) {
         // if you're on a page that no longer has data, go to last page with data
-        if ( jobs.length === 0 && (this.paging.getCurrentPage() >= this.getPages()) ) {
+        if ( jobs.length === 0 && (this.paging.getCurrentPage() >= this.getPages())
+            // or your current page is greater than the number of pages
+            // like when you change the page size from the last page
+            || (this.paging.getCurrentPage() > this.getPages())) {
             this.paging.setPage( this.getPages() );
         }
 
