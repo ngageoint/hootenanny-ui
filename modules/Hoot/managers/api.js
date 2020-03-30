d804d3017bff942860db957cdc03e9fe096a9eeb
Brian Hatchl <brian.hatchl@maxar.com>
2020-03-30 13:02:46 -0400

    use proper content type

--- modules/Hoot/managers/api.js
+++ modules/Hoot/managers/api.js
@@ -491,13 +491,12 @@ export default class API {
             } );
     }
 
-    // TODO: remove this if not needed
     getTileNodesCount( data ) {
         const params = {
             path: '/osm/api/0.6/map/nodescount',
             method: 'POST',
             headers: {
-                'Content-Type': 'text/plain'
+                'Content-Type': 'application/json'
             },
             data
         };
