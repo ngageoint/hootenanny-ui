28eff31f57be05bf878da621ad23b4e4a0673ff8
Brian Hatchl <brian.hatchl@maxar.com>
2022-01-20 21:31:28 -0500

    fix running in dev

--- webpack-config/webpack.base.config.js
+++ webpack-config/webpack.base.config.js
@@ -28,7 +28,7 @@ module.exports = {
     output: {
         path: resolve( __dirname, '../dist/' ),
         filename: '[name].min.js',
-        publicPath: './'
+        publicPath: '/'
     },
     module: {
         rules: [
