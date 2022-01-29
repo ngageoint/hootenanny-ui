0ae2776f806e88b7e0c2348888508c73b8f9a014
Brian Hatchl <brian.hatchl@maxar.com>
2022-01-29 11:33:40 -0500

    split chunks was causing an error loading modules

--- webpack-config/webpack.production.config.js
+++ webpack-config/webpack.production.config.js
@@ -27,21 +27,13 @@ module.exports = merge( CommonConfig, {
         ]
     },
     optimization: {
-        splitChunks: {
-            chunks: 'initial',
-            name: (module, chunks, cacheGroupKey) => {
-                const allChunksNames = chunks.map((chunk) => chunk.name).join('~');
-                const prefix = cacheGroupKey === 'defaultVendors' ? 'vendors' : cacheGroupKey;
-                return `${prefix}~${allChunksNames}`;
-              },
-        },
         minimize: true,
         minimizer: [
             // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
             // `...`,
             new TerserPlugin(),
             new CssMinimizerPlugin(),
-        ]
+        ],
     },
 
     plugins: [
