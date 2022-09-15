b808bb86d45c419cd76c640be219a58dad26b085
Brian Hatchl <brian.hatchl@maxar.com>
2022-09-15 11:34:47 -0400

    fix test

--- test/spec/util/detect.js
+++ test/spec/util/detect.js
@@ -1,5 +1,5 @@
 describe('iD.detect', function() {
-    var detect = iD.utilDetect();
+    var detect = iD.Detect();
     describe('absolute', function() {
         it('get the absolute url for dev webpack', function() {
             var base = 'https://localhost:8080/';
@@ -11,13 +11,13 @@ describe('iD.detect', function() {
             var base = 'https://localhost:8443/hootenanny-id/';
             var rel = '../hoot-services';
             var abs = detect.absolute(base, rel);
-            expect(abs).to.eql('https://localhost:8080/hoot-services');
+            expect(abs).to.eql('https://localhost:8443/hoot-services');
         });
         it('get the absolute url for production', function() {
             var base = 'https://localhost/hootenanny/hootenanny-id/';
             var rel = '../hoot-services';
             var abs = detect.absolute(base, rel);
-            expect(abs).to.eql('https://localhost:8080/hoot-services');
+            expect(abs).to.eql('https://localhost/hootenanny/hoot-services');
         });
     });
 });
